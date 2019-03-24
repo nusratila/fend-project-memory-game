18006615aa2c6c8ac5110c8ffeaf093ff51df841
Nusrat Jahan Ila <nusrat.ila@gmail.com>
2019-03-24 16:23:14 -0700

    added 5 stars

--- js/app.js
+++ js/app.js
@@ -9,7 +9,9 @@ let currentId = -1;
 let currentIcon ="none";
 let matchCount = 0;
 let totalSeconds = 0;
-let setInId = -1;
+let setInId =  -1;
+let finishTime = "none";
+let starCount = -1;
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function initialize(){
@@ -29,9 +31,14 @@ function initialize(){
     card.className= "fa " + allCards[i];
     cards[i].id= "card-li-"+i;
     cards[i].className="card";
+    
     };
 
+    secondsLabel.innerHTML = "Time:00:00:00" ;
+
    clearInterval(setInId);
+   assignStar();
+
 };
 
 let secondsLabel = document.querySelector(".timer");
@@ -41,8 +48,9 @@ function setTime() {
     let minute = parseInt(totalSeconds/60); // it can go above 60
     let minutepart = minute % 60; //it will be less than 60 always
     let hour = parseInt(minute/60); 
+    finishTime = hour.toString().padStart(2, '0') + ":" + minutepart.toString().padStart(2, '0') + ":" + second.toString().padStart(2, '0');
 
-    secondsLabel.innerHTML = "Time: " + hour.toString().padStart(2, '0') + ":" + minutepart.toString().padStart(2, '0') + ":" + second.toString().padStart(2, '0');
+    secondsLabel.innerHTML = "Time: " +  finishTime ;
 
 }
 
@@ -76,12 +84,63 @@ function showCard(ele){
         totalSeconds = 0;
         setInId= setInterval(setTime, 1000);
     }
+    assignStar();
 
     let moves = document.querySelector(".moves");
     moves.innerHTML=clickCount;
     console.log(clickCount);
 
 };
+
+function changeStar(n){
+    
+    const ulStars = document.querySelector("ul.stars");
+    let stars = ulStars.querySelectorAll("LI");
+
+    for (let i =0; i<5; i=i+1){
+        let star = stars[i].querySelector("I");
+       
+        if (i<n) {
+            star.className= "fa fa-star-o" ;
+        }
+
+      else{
+        star.className = "fa fa-star";
+      }
+        
+        
+        };
+    
+}
+
+function assignStar(){
+    if(clickCount > 50){
+        changeStar(4);
+        starCount=4;
+    }
+
+    else if(clickCount > 40){
+        changeStar(3);
+        starCount=3;
+    }
+
+    else if(clickCount > 30){
+        changeStar(2);
+        starCount=2;
+    }
+
+    else if(clickCount > 20){
+        changeStar(1);
+        starCount=1;
+    }
+
+    else{
+        changeStar(0);
+        starCount=0;
+    }
+
+}
+
 
 function matchCard(liid){
     let ele = document.querySelector("#"+liid);
@@ -99,7 +158,8 @@ function hideCard(liid){
 
 function checkGameOver(){
     if (matchCount == 8){
-        console.log("congratulations,Game over");
+        alert("congratulations, total star : " + (5-starCount) + " time: " + finishTime );
+        clearInterval(setInId);
     }
 
 };
